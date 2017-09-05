52306cb5665838f3ce52b419131940a4521a19ff
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-09-05 09:37:12 -0500

    refactor, clean, lint

--- commands/unzipStatic.js
+++ commands/unzipStatic.js
@@ -1,129 +1,89 @@
-const forceUtils = require('../lib/forceUtils.js');
-const {
-  exec
-} = require('child_process');
-const fs = require('fs');
-const fse = require('fs-extra');
-const path = require('path');
+//const forceUtils = require('../lib/forceUtils.js');
+// const {
+//   exec
+// } = require('child_process');
+const fs = require("fs");
+//const fse = require("fs-extra");
+//const path = require("path");
 const decompress = require("decompress");
+const _ = require("lodash");
 const decompressUnzip = require("decompress-unzip");
-const parseXML = require('xml2js').parseString;
+//const parseXML = require("xml2js").parseString;
+const walkSync = require("../lib/staticCommon.js").walkSync;
 
 (function () {
-  'use strict';
+  "use strict";
 
   module.exports = {
-    topic: 'static',
-    command: 'unzip',
-    description: 'unzip static resources',
-    help: 'unzips static resource(s) of type application/zip to a local folder'
+    topic: "static",
+    command: "unzip",
+    description: "unzip static resources",
+    help: "unzips static resource(s) of type application/zip to a local folder called resource-bundles.  This will reside at the top level of your sfdx project."
     /*flags: [{
-      name: 'name',
-      char: 'n',
-      description: 'static resource name (omit to do all of them)',
+      name: "name",
+      char: "n",
+      description: "static resource name (omit to do all of them)",
       hasValue: true,
       required: false
     }, {
-      name: 'outputdir',
-      char: 'd',
-      description: 'folder for saving the created files',
+      name: "outputdir",
+      char: "d",
+      description: "folder for saving the created files",
       hasValue: true,
       required: false
     }]*/,
     run(context) {
       var topLevelAbsolute, config;
-      const unzippedLocation = 'resource-bundles';
+      const unzippedLocation = "resource-bundles";
 
-
-      const target = context.flags.name;
-      const outputdir = context.flags.outputdir;
-
-      var walkSync = function(dir, filelist) {
-        var fs = fs || require('fs'),
-            files = fs.readdirSync(dir);
-        filelist = filelist || [];
-        files.forEach(function(file) {
-          if (fs.statSync(dir + file).isDirectory()) {
-            filelist = walkSync(dir + file + '/', filelist);
-          }
-          else {
-            if (file.includes(".resource") && !file.includes(".resource-meta.xml")){
-              //see if the file is zipped type?
-              var fullpath = dir  + file;
-              var meta = fs.readFileSync(fullpath + "-meta.xml", 'utf8');
-              //console.log(parseString(meta));
-              //console.log(fullpath);
-              //console.log(meta);
-              parseXML(meta, function(err, result){
-                if (err){
-                  //console.log(err)
-                } else {
-                  //console.log(result.StaticResource.contentType[0]);
-                  if (result.StaticResource.contentType){
-                    //console.log(result.StaticResource.contentType);
-                    if (result.StaticResource.contentType[0]==='application/zip'){
-                      //console.log(fullpath);
-                      filelist.push(fullpath);
-                    } else {
-                      //console.log("not a zipped file!")
-                    }
-                  }
-                }
-              })
-            }
-          }
-        });
-        return filelist;
-      };
+      //const target = context.flags.name;
+      //const outputdir = context.flags.outputdir;
 
       while (!topLevelAbsolute){
-        console.log('Starting directory: ' + process.cwd());
-        if (fs.existsSync('sfdx-project.json')){
-          //console.log("found config file!");
+        if (fs.existsSync("sfdx-project.json")){
           topLevelAbsolute = process.cwd();
-          var config = JSON.parse(fs.readFileSync('sfdx-project.json', 'utf8'));
-          //console.log(config);
-
-          //create the folder.  Rename it up higher if you don't like resource-bundles
-          if (!fs.existsSync(unzippedLocation)){
-            fs.mkdirSync(unzippedLocation);
-          }
-
-          //find some static resources
-          var allFiles = walkSync(topLevelAbsolute+'/');
-
-          //console.log(allFiles);
-          //for each of the zip files
-          for (var i=0; i<allFiles.length; i++){
-            //console.log(allFiles[i]);
-            var fullRelative = allFiles[i].replace(topLevelAbsolute+'/', '').replace(".resource", '');
-            //console.log(fullRelative);
-            var paths = fullRelative.split('/');
-            var pathInProgress = 'resource-bundles';
-            //recreate the directory structure from the source tree(s)
-            for (var k=0; k<paths.length; k++){
-              pathInProgress = pathInProgress + '/' + paths[k]
-              //console.log(pathInProgress);
-              if (!fs.existsSync(pathInProgress)){
-                fs.mkdirSync(pathInProgress);
-              }
-            }
-            //console.log("destination folder: " +  unzippedLocation + '/' +  fullRelative );
-            //fs.createReadStream(allFiles[i]).pipe(unzip.Extract({ path: unzippedLocation + '/' +  fullRelative}));
-            //nodeJsZip.unzip(path.join(__dirname,"./demo.zip"),callback);
-            decompress(allFiles[i], unzippedLocation + '/' +  fullRelative, {
-              plugins: [
-                  decompressUnzip()
-              ]
-            });
-            console.log('Files decompressed to ' + fullRelative);
-          }
-
         } else {
           console.log("no config file. going up a directory");
-          process.chdir('..');
+          process.chdir("..");
         }
       }
+
+      //ok, we're in top-level.  Read the config file to see what directories we need to look in.
+      config = JSON.parse(fs.readFileSync("sfdx-project.json", "utf8"));
+      //console.log(config);
+
+      //create the folder.  Rename it up higher if you don't like resource-bundles
+      if (!fs.existsSync(unzippedLocation)){
+        fs.mkdirSync(unzippedLocation);
+      }
+
+      //find some static resources in package directories
+      var allFiles = [];
+      _.forEach(config.packageDirectories, function(pd){
+        _.merge(allFiles, walkSync(topLevelAbsolute + "/" + pd.path + "/"));
+      });
+
+      //for each of the zip files
+      _.forEach(allFiles, function(file){
+        var fullRelative = file.replace(topLevelAbsolute+"/", "").replace(".resource", "");
+        //console.log(fullRelative);
+        var paths = fullRelative.split("/");
+        var pathInProgress = unzippedLocation;
+        //recreate the directory structure from the source tree(s)
+        _.forEach(paths, function(pathSlice){
+          pathInProgress = pathInProgress + "/" + pathSlice;
+          //console.log(pathInProgress);
+          if (!fs.existsSync(pathInProgress)){
+            fs.mkdirSync(pathInProgress);
+          }
+        });
+
+        decompress(file, unzippedLocation + "/" +  fullRelative, {
+          plugins: [ decompressUnzip() ]
+        });
+
+        console.log("Files decompressed to " + fullRelative);
+      });
     }
   };
 }());
--- commands/zipStatic.js
+++ commands/zipStatic.js
@@ -1,109 +1,97 @@
-const forceUtils = require('../lib/forceUtils.js');
-const {
+//const forceUtils = require('../lib/forceUtils.js');
+/*const {
   exec
-} = require('child_process');
-const fs = require('fs');
-const fse = require('fs-extra');
-const path = require('path');
-const parseXML = require('xml2js').parseString;
-const archiver = require('archiver');
+} = require('child_process');*/
+const fs = require("fs");
+//const fse = require("fs-extra");
+//const path = require("path");
+//const parseXML = require("xml2js").parseString;
+const archiver = require("archiver");
+const _ = require("lodash");
+const walkSync = require("../lib/staticCommon.js").walkSync;
 
 
 (function () {
-  'use strict';
+  "use strict";
 
   module.exports = {
-    topic: 'static',
-    command: 'zip',
-    description: 'zip static resources',
-    help: 'zips static resource(s) of type application/zip from local source folder to their original location in the tree',
+    topic: "static",
+    command: "zip",
+    description: "zip static resources",
+    help: "zips static resource(s) of type application/zip from local source folder to their original location in the tree",
     run(context) {
       var topLevelAbsolute, config;
-      const unzippedLocation = 'resource-bundles';
+      const unzippedLocation = "resource-bundles";
 
+      //const target = context.flags.name;
+      //const outputdir = context.flags.outputdir;
 
-      const target = context.flags.name;
-      const outputdir = context.flags.outputdir;
+      // var walkSync = function(dir, filelist) {
+      //   var files = fs.readdirSync(dir);
+      //   filelist = filelist || [];
+      //   files.forEach(function(file) {
+      //     if (fs.statSync(dir + file).isDirectory()) {
+      //       filelist = walkSync(dir + file + "/", filelist); //recursive walk of any directories found
+      //     } else if (file.includes(".resource") && !file.includes(".resource-meta.xml")){ //see if the file is zipped type?
 
-      var walkSync = function(dir, filelist) {
-        var fs = fs || require('fs'),
-            files = fs.readdirSync(dir);
-        filelist = filelist || [];
-        files.forEach(function(file) {
-          if (fs.statSync(dir + file).isDirectory()) {
-            filelist = walkSync(dir + file + '/', filelist);
-          }
-          else {
-            if (file.includes(".resource") && !file.includes(".resource-meta.xml")){
-              //see if the file is zipped type?
-              var fullpath = dir  + file;
-              var meta = fs.readFileSync(fullpath + "-meta.xml", 'utf8');
-              //console.log(parseString(meta));
-              //console.log(fullpath);
-              //console.log(meta);
-              parseXML(meta, function(err, result){
-                if (err){
-                  //console.log(err)
-                } else {
-                  //console.log(result.StaticResource.contentType[0]);
-                  if (result.StaticResource.contentType){
-                    //console.log(result.StaticResource.contentType);
-                    if (result.StaticResource.contentType[0]==='application/zip'){
-                      //console.log(fullpath);
-                      filelist.push(fullpath);
-                    } else {
-                      //console.log("not a zipped file!")
-                    }
-                  }
-                }
-              })
-            }
-          }
-        });
-        return filelist;
-      };
+      //         var fullpath = dir  + file;
+      //         var meta = fs.readFileSync(fullpath + "-meta.xml", "utf8");
+
+      //         parseXML(meta, function(err, result){
+      //           if (err){
+      //             console.log(err);
+      //           } else {
+      //             //console.log(result.StaticResource.contentType[0]);
+      //             if (result.StaticResource.contentType &&  result.StaticResource.contentType[0]==="application/zip"){
+      //               filelist.push(fullpath);
+      //             }
+      //           }
+      //         });
+      //     }
+      //   });
+      //   return filelist;
+      // };
+
 
       while (!topLevelAbsolute){
-        console.log('Starting directory: ' + process.cwd());
-        if (fs.existsSync('sfdx-project.json')){
-          //console.log("found config file!");
+        if (fs.existsSync("sfdx-project.json")){
           topLevelAbsolute = process.cwd();
-          var config = JSON.parse(fs.readFileSync('sfdx-project.json', 'utf8'));
-          //console.log(config);
-
-          //create the folder.  Rename it up higher if you don't like resource-bundles
-          if (!fs.existsSync(unzippedLocation)){
-            //fs.mkdirSync(unzippedLocation);
-            console.log("resource-bundles folder was not found");
-            process.exit(1);
-          }
-
-          //find some static resources
-          var allFiles = walkSync(topLevelAbsolute+'/');
-
-          //console.log(allFiles);
-          //for each of the static resource files
-          for (var i=0; i<allFiles.length; i++){
-            //create the path from its resource-bundle
-            var resourceDir = unzippedLocation + '/' + allFiles[i].replace(topLevelAbsolute+'/', '').replace(".resource", '');
-            console.log("will compress:");
-
-            var output = fs.createWriteStream(allFiles[i]);
-            var archive = archiver('zip', {
-                zlib: { level: 9 } // Sets the compression level.
-            });
-
-            archive.pipe(output);
-            archive.directory(resourceDir, false);
-          }
-          archive.finalize();
-
-
         } else {
           console.log("no config file. going up a directory");
-          process.chdir('..');
+          process.chdir("..");
         }
       }
+
+      //check for existence of folder.  If you can't find it, that's a problem
+      if (!fs.existsSync(unzippedLocation)){
+        throw new Error("resource-bundles folder was not found");
+      }
+
+      config = JSON.parse(fs.readFileSync("sfdx-project.json", "utf8"));
+
+      //find some static resources
+      var allFiles = [];
+      _.forEach(config.packageDirectories, function(pd){
+        _.merge(allFiles, walkSync(topLevelAbsolute + "/" + pd.path + "/"));
+      });
+
+      //for each of the static resource files
+      var archive = archiver("zip", {
+        zlib: { level: 9 } // Sets the compression level.
+      });
+
+      _.forEach(allFiles, function(file){
+        var resourceDir = unzippedLocation + "/" + file.replace(topLevelAbsolute+"/", "").replace(".resource", "");
+        //console.log("will compress:");
+
+        var output = fs.createWriteStream(file);
+
+        archive.pipe(output);
+        archive.directory(resourceDir, false);
+      });
+
+      archive.finalize();
+
     }
   };
 }());
